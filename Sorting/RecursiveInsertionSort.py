b79d7efb4468adf9775d31734b78806f1b96f1c8
Piyush Maji <piyushmaji6595@gmail.com>
2026-03-23 03:10:19 +0530

    new file recursiveInsertionSort.py

--- Sorting/RecursiveInsertionSort.py
+++ Sorting/RecursiveInsertionSort.py
@@ -1,18 +1,19 @@
 class Solution:
-    def recursiveInsertionSort(arr,i,n):
-        if i>=n:
+    def recursiveInsertionSort(self,arr,i,n):
+        if n==1:
             return
-        
+        if i==n:
+            return
         key = arr[i]
-        j = i-1
+        j=i-1
         while j>=0 and arr[j]>key:
-            arr[j+1] = arr[j]
+            arr[j+1]=arr[j]
             j-=1
-        arr[j+1] = key
+        arr[j+1]=key
+        self.recursiveInsertionSort(arr,i+1,n)
 
-        Solution.recursiveInsertionSort(arr,i+1,n)
         return arr
+    
 a = Solution()
 arr1 = [5, 3, 2, 8,1]
-print(Solution.recursiveInsertionSort(arr1, 1, len(arr1)))
-# The recursive insertion sort algorithm works by recursively sorting the first i-1 elements of the list
+print(a.recursiveInsertionSort(arr1, 0, len(arr1)))
