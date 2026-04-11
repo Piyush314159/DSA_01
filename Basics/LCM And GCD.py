f345a374b19afddd8efe57cf36981ea9db6acc2e
Piyush Maji <piyushmaji6595@gmail.com>
2026-04-11 11:47:40 +0530

    chnage is lcm_gcd code

--- Basics/LCM And GCD.py
+++ Basics/LCM And GCD.py
@@ -1,15 +1,13 @@
 class Solution:
     def lcmAndGcd(self, a : int, b : int):
-
-        int_len = len(str(a)) if len(str(a))>len(str(b)) else len(str(b))
-        dict_array_gcd_lcm={}
-        for i in range(2,10**int_len):
+        range_ = a if a<b else b
+        gcd = 0
+        for i in range(1,range_+1):
             if a%i==0 and b%i==0:
-                a_rem = int(a/i)
-                b_rem = int(b/i)
-                dict_array_gcd_lcm[i*a_rem*b_rem]=[i*a_rem*b_rem,i]
-            else:
-                dict_array_gcd_lcm[1*a*b]=[1*a*b,1]
-        return dict_array_gcd_lcm[min(dict_array_gcd_lcm)]
-a=Solution()
-print(a.lcmAndGcd(10,5))
+                gcd = i
+
+        lcm = (a*b)//gcd
+        return [lcm,gcd]
+    
+a = Solution()
+print(a.lcmAndGcd(12,16))
