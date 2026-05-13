3e829744d8dc481454181168413b5ede0458fde4
Piyush Maji <piyushmaji6595@gmail.com>
2026-05-13 15:12:03 +0530

    update search2Dmatrix.py with a more efficient search method that directly checks the elements in the 2D matrix without flattening it into a 1D array. This will reduce the time complexity from O(m*n) to O(m+n) where m is the number of rows and n is the number of columns in the matrix.

--- Arrays/search2Dmatrix.py
+++ Arrays/search2Dmatrix.py
@@ -1,16 +1,10 @@
 #User function Template for python3
 class Solution:
 	def matSearch(self, mat, x):
-		flat_arr = []
-            
-		for row in mat:
-			for el in row:
-				flat_arr.append(el)
-		cols = len(mat[0])
-				
-		for i in range(len(flat_arr)):
-			if flat_arr[i]==x:
-				return[i//cols,i%cols]
+		for i in range(len(mat)):
+			for j in range(len(mat[0])):
+				if mat[i][j]==x:
+					return [i,j]
 			
 a = Solution()
 print(a.matSearch( [[3, 30, 38],[20, 52, 54],[35, 60, 69]], 35))
