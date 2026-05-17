9b5566fd5fd1a3e3415b55d524d6c6c640f177a8
Piyush Maji <piyushmaji6595@gmail.com>
2026-05-17 10:15:01 +0530

    new file:   Arrays/Medium/nextPermutation.py

---
+++ Arrays/Medium/nextPermutation.py
@@ -0,0 +1,49 @@
+'''
+* Finding pivot → O(n)
+* Finding swap element → O(n)
+* Reversing suffix → O(n)
+'''
+class Solution:  #arr[] = [2, 4, 1, 7, 5, 0]
+    def nextPermutation(self, arr):
+        n = len(arr)
+        i=n-2
+        j=n-1
+        pivot = -1
+
+        #finding the pivot
+        while i>=0:
+            if arr[i]<arr[i+1]:
+                pivot = i
+                break
+            i-=1
+
+        #edge case
+        if pivot==-1:
+            l = 0
+            r = n-1
+            while l<r:
+                arr[l], arr[r] = arr[r], arr[l]
+                l+=1
+                r-=1
+
+        else:
+            #finding the swaping element
+            while j>pivot:
+                if arr[j]>arr[i]:
+                    swap = j
+                    break
+                j-=1
+            arr[pivot], arr[swap] = arr[swap], arr[pivot]
+
+            #now reversing suffix elements
+            l = i+1
+            r = n-1
+            while l<r:
+                arr[l], arr[r] = arr[r], arr[l]
+                l+=1
+                r-=1
+
+        return arr
+    
+a = Solution()
+print(a.nextPermutation([3, 2, 1]))
