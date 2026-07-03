d753ff4e2f428f381970c3977958c2302885d932
Piyush Maji <piyushmaji6595@gmail.com>
2026-07-03 23:20:12 +0530

    new file:   Strings/easy/validAngaram.py

---
+++ Strings/easy/validAngaram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_dict = {}
+        for ch in s:
+            s_dict[ch] = s_dict.get(ch, 0) + 1
+        for ch in t:
+            if s_dict.get(ch,0) == 0:
+                return False
+            s_dict[ch] -= 1
+        return True
+    
+a = Solution()
+print(a.isAnagram("anagram", "nagaram"))  # True
+print(a.isAnagram("rat", "car"))  # False
