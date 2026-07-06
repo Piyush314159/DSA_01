6f6b47701daad145f45228cf6628fe3e9848a3c1
Piyush Maji <piyushmaji6595@gmail.com>
2026-07-06 19:10:37 +0530

    new file roman to int

---
+++ Strings/medium/romanToIntegers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_dict = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+        }
+        res = 0
+        for i in range(len(s) -1):
+            if roman_dict[s[i]] < roman_dict[s[i+1]]:
+                res -= roman_dict[s[i]]
+            else:
+                res += roman_dict[s[i]]
+        return res + roman_dict[s[-1]]
+a = Solution()
+print(a.romanToInt('MDMCMXCIV'))
