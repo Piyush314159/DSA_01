02590a2a0047417a1c5b1de83733d81ebd498182
Piyush Maji <piyushmaji6595@gmail.com>
2026-07-19 13:17:04 +0530

    new problem re-arranging the linked list in odd and even positions

---
+++ LinkedList/Medium/oddEvenLL.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Solution:
+    def oddEven(self, head):
+        if not head:
+            return head
+        
+        odd_dummy = Node(-1)
+        even_dummy = Node(-1)
+        odd_tail = odd_dummy
+        even_tail = even_dummy
+
+        curr = head
+        while curr:
+            if curr.data % 2 == 1:
+                odd_tail.next = curr
+                odd_tail = odd_tail.next
+            else:
+                even_tail.next = curr
+                even_tail = even_tail.next
+            curr = curr.next
+
+        even_tail.next = None
+        odd_tail.next = even_dummy.next
+        
+        return odd_dummy.next
+    
+def print_linked_list(head):
+    curr = head
+    while curr:
+        if curr.next:
+            print(curr.data, end=" -> ")
+        else:
+            print(curr.data, end=" -> None")
+        curr = curr.next
+    
+# creating a linked list for testing
+# ===================================
+head = Node(1)
+head.next = Node(4)
+head.next.next = Node(2)
+head.next.next.next = Node(3)
+head.next.next.next.next = Node(5)
+head.next.next.next.next.next = Node(7)
+head.next.next.next.next.next.next = Node(8)
+
+print_linked_list(head)
+
+sol = Solution()
+head1 = sol.oddEven(head)
+
+print("\nAfter rearranging odd and even nodes:")
+print_linked_list(head1)
