52fcf8681ade10924d6b5e5953fe630edbb96a9c
Piyush Maji <piyushmaji6595@gmail.com>
2026-07-25 17:51:52 +0530

    new problem remove duplicates from a doubly linked list

---
+++ LinkedList/DoublyLinkedList.py/medium/removeDuplicatesDLL.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+        self.prev = None
+
+class Solution:
+    def deleteDuplicates(self, head):
+        seen = set()
+        curr = head
+        while curr:
+            if curr.data in seen:
+                if curr.prev:
+                    curr.prev.next = curr.next
+                if curr.next:
+                    curr.next.prev = curr.prev
+            else:
+                seen.add(curr.data)
+            curr = curr.next
+        return head
+
+def printList(head):
+    curr = head
+    while curr:
+        if curr.next:
+            print(curr.data, end=" <-> ")
+        else:
+            print(curr.data, end=" -> None")
+        curr = curr.next
+    print()
+
+#create a doubly linked list for testing
+head = Node(1)
+node2 = Node(2)
+node3 = Node(2)
+node4 = Node(3)
+node5 = Node(4)
+node6 = Node(4)
+node7 = Node(5)
+head.next = node2
+node2.prev = head
+node2.next = node3
+node3.prev = node2
+node3.next = node4
+node4.prev = node3
+node4.next = node5
+node5.prev = node4
+node5.next = node6
+node6.prev = node5
+node6.next = node7
+node7.prev = node6
+
+print("Original list:")
+printList(head)
+sol = Solution()
+head = sol.deleteDuplicates(head)
+print("List after removing duplicates:")
+printList(head)
